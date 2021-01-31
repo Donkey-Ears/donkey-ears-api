e903f2da50ad5ca260c4141e50e3d1d731b97bba
Marco Kang <kmnkit@gmail.com>
2021-02-01 00:10:58 +0900

    clean push

--- shouts/views.py
+++ shouts/views.py
@@ -1,8 +1,11 @@
 from rest_framework.viewsets import ModelViewSet
 from .models import Shout
 from .serializers import ShoutSerializer
+from .permissions import IsAuthor
+from rest_framework.permissions import IsAdminUser
 
 
 class ShoutViewSet(ModelViewSet):
     queryset = Shout.objects.select_related("user").all()
     serializer_class = ShoutSerializer
+    permission_classes = [IsAuthor, IsAdminUser]
